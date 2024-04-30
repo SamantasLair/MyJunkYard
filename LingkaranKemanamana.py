f20d1ceab2429abfd53858ebe92db117ecfb25c2
Samuel Ananta <94610303+SamantasLair@users.noreply.github.com>
2024-04-30 22:55:59 +0700

    Numero uno

---
+++ LingkaranKemanamana.py
@@ -0,0 +1,20 @@
+from manim import *
+
+
+class MyAnimation(Scene):
+    def construct(self):
+        plane = NumberPlane()
+        self.add(plane)
+        self.wait(1)
+        circle = Circle()
+
+        self.play(Create(circle))
+
+        # Move the circle around the screen
+        for x in range(-4, 5):
+            self.play(ApplyMethod(circle.move_to, np.array([x, x, 10-x])))
+            self.play(ApplyMethod(circle.move_to, np.array([x-2, x, 10-x])))
+            self.play(ApplyMethod(circle.move_to, np.array([x+1, x, 10-x])))
+
+# To run the animation, add the following line:
+MyAnimation().render()
